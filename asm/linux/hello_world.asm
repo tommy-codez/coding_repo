3b8408cff46b713661855681b4a585ace3eda3fd
Thomas Saar <t-saar@gmx.de>
2020-07-04 15:51:29 +0200

    Linux ASM

---
+++ asm/linux/hello_world.asm
@@ -0,0 +1,16 @@
+
+global _start
+section .data
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
+
+section .text
+_start:
+  mov rax, 1 ; write
+  mov rdi, 1 ; stdout
+  mov rsi, msg
+  mov rdx, msg.len
+  syscall
+  mov rax, 60 ; exit
+  mov rdi, 0
+  syscall
